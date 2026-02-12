9fb64fd3ba09a2d04c50759503a7e98627b98ceb
Rabiya <rabiya@gmail.com>
2026-02-12 23:28:45 +0500

    ......

--- base/header.php
+++ base/header.php
@@ -21,6 +21,9 @@ if (session_status() === PHP_SESSION_NONE) {
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700&display=swap" rel="stylesheet">
 
+    <!-- Bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     
     <!-- Css Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/sound/css/bootstrap.min.css" type="text/css">
